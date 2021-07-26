5256c3eb34d2ab4fcaa2689973d22571c66fa019
Myllena Cruz <myllena12345@hotmail.com>
2021-07-26 17:59:31 -0300

    Validações

--- models/clientservices.js
+++ models/clientservices.js
@@ -5,16 +5,39 @@ class ClientService {
   add(clientservice, res) {
     const creationDate = moment().format('YYYY-MM-DD HH:mm:ss')
     const date = moment(clientservice.date, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss')
-    const datedService = { ...clientservice, creationDate, date }
+    
+    const validDate = moment(date).isSameOrAfter(creationDate)
+    const validClient = clientservice.client.lenght >= 5
 
-    const sql = 'INSERT INTO ClientServices SET?'
-    connection.query(sql, datedService, (err, results) => {
-      if (err) {
-        res.status(400).json(err)
-      } else {
-        res.status(201).json(results)
+    const validations = [
+      {
+        name: 'date',
+        valide: validDate,
+        message: 'Date must be bigger or equal to current date'
+      },
+      {
+        name: 'client',
+        valid: validClient,
+        message: 'Client must have at least 5 characters'
       }
-    })
+    ]
+
+    const errs = validations.filter(field => !field.valid)
+    const existErrs =  errs.length
+    if(existErrs) {
+      res.status(400).json(errs)
+    } else {
+      const datedService = { ...clientservice, creationDate, date }
+
+      const sql = 'INSERT INTO ClientServices SET?'
+      connection.query(sql, datedService, (err, results) => {
+        if (err) {
+          res.status(400).json(err)
+        } else {
+          res.status(201).json(results)
+        }
+      })
+    }
   }
 }
 
